ed5dcb079c7d6d560dfc36b35356027adafde3de
Gavin Henry <ghenry@suretec.co.uk>
2023-04-17 11:47:37 +0100

    Backport -- Merge pull request #260 from reasig/update-phoenix-html

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Kaffy.MixProject do
     [
       {:phoenix, "~> 1.4"},
       {:phoenix_html, "~> 2.13 or ~> 3.0"},
-      {:mock, "~> 0.3.0", only: :test},
+      {:mock, "~> 0.3.3", only: :test},
       {:ecto, "~> 3.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
